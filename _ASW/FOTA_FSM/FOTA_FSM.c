0f84ca5c5880f9bce5ad538591f9ab697c1b0650
shivamchudasama <shivamchudasama@gmail.com>
2026-02-13 00:07:06 +0530

    - Created: FOTA_FSM for integration of SMF.

---
+++ _ASW/FOTA_FSM/FOTA_FSM.c
@@ -0,0 +1,150 @@
+/**
+ * @file          FOTA_FSM.c
+ * @brief         Source file containing SMF implementation for FOTA service.
+ * @date          13/02/2026
+ * @author        Shivam Chudasama [SC]
+ * @copyright     Bajaj Auto Technology Limited (BATL)
+ */
+
+/******************************************************************************/
+/*                                                                            */
+/*                                  INCLUDES                                  */
+/*                                                                            */
+/******************************************************************************/
+#include "FOTA_FSM.h"
+
+/******************************************************************************/
+/*                                                                            */
+/*                                  DEFINES                                   */
+/*                                                                            */
+/******************************************************************************/
+/**
+ * @def           <Define name>
+ * @brief         <Define details>.
+ */
+
+/******************************************************************************/
+/*                                                                            */
+/*                                   ENUMS                                    */
+/*                                                                            */
+/******************************************************************************/
+// Definition of all the enums
+/**
+ * @enum          <Enum name>
+ * @brief         <Enum details>.
+ */
+
+// Declarations of all the enum variables
+/**
+ * @var           <Variable name>
+ * @brief         <Variable details>.
+ */
+
+/******************************************************************************/
+/*                                                                            */
+/*                                 STRUCTURES                                 */
+/*                                                                            */
+/******************************************************************************/
+// Definition of all the structures
+/**
+ * @struct        <Structure name>
+ * @brief         <Structure details>.
+ */
+
+// Declarations of all the structure variables
+/**
+ * @var           <Variable name>
+ * @brief         <Variable details>.
+ */
+
+/******************************************************************************/
+/*                                                                            */
+/*                                   UNIONS                                   */
+/*                                                                            */
+/******************************************************************************/
+// Definition of all the unions
+/**
+ * @union         <Union name>
+ * @brief         <Union details>.
+ */
+
+// Declarations of all the union variables
+/**
+ * @var           <Variable name>
+ * @brief         <Variable details>.
+ */
+
+/******************************************************************************/
+/*                                                                            */
+/*                       PRIVATE FUNCTION DECLARATIONS                        */
+/*                                                                            */
+/******************************************************************************/
+
+/******************************************************************************/
+/*                                                                            */
+/*                              EXTERN VARIABLES                              */
+/*                                                                            */
+/******************************************************************************/
+
+/******************************************************************************/
+/*                                                                            */
+/*                              PUBLIC VARIABLES                              */
+/*                                                                            */
+/******************************************************************************/
+/**
+ * @var           <Variable name>
+ * @brief         <Variable details>.
+ */
+
+/******************************************************************************/
+/*                                                                            */
+/*                             PRIVATE VARIABLES                              */
+/*                                                                            */
+/******************************************************************************/
+/**
+ * @var           <Variable name>
+ * @brief         <Variable details>.
+ */
+
+/******************************************************************************/
+/*                                                                            */
+/*                              EXTERN FUNCTIONS                              */
+/*                                                                            */
+/******************************************************************************/
+
+/******************************************************************************/
+/*                                                                            */
+/*                        PRIVATE FUNCTION DEFINITIONS                        */
+/*                                                                            */
+/******************************************************************************/
+/**
+ * @private       <Function name>
+ * @brief         <Function details>.
+ * @param[in]     <Input parameter details>.
+ * @param[out]    <Output parameter details>.
+ * @param[inout]  <Input-Output parameter details>.
+ * @return        <Return details>.
+ */
+
+/******************************************************************************/
+/*                                                                            */
+/*                        PUBLIC FUNCTION DEFINITIONS                         */
+/*                                                                            */
+/******************************************************************************/
+/**
+ * @public        <Function name>
+ * @brief         <Function details>.
+ * @param[in]     <Input parameter details>.
+ * @param[out]    <Output parameter details>.
+ * @param[inout]  <Input-Output parameter details>.
+ * @return        <Return details>.
+ */
+
+/**
+ * Copyright(c) Bajaj Auto Technology Limited (BATL) as an unpublished work.
+ * THIS SOFTWARE AND/OR MATERIAL IS THE PROPERTY OF BATL.
+ * ALL USE, DISCLOSURE, AND/OR REPRODUCTION NOT SPECIFICALLY AUTHORIZED BY
+ * BATL IS PROHIBITED.
+ *
+ * @author:Shivam Chudasama [SC]
+ */
