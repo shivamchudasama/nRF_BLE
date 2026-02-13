fa1fdfe3b981d4a792da17c2b5e4b7f56ebe6247
Shivam <shivamchudasama@gmail.com>
2026-02-13 20:21:12 +0530

    - Added: FOTA TP related files.

---
+++ _ASW/BLE_FOTA/BLE_FOTA_TP/FOTA_TP.h
@@ -0,0 +1,84 @@
+/**
+ * @file          FOTA_TP.h
+ * @brief         Header file containing FOTA Transport Protocol definitions.
+ * @date          13/02/2026
+ * @author        Shivam Chudasama [SC]
+ * @copyright     Bajaj Auto Technology Limited (BATL)
+ */
+
+#ifndef _FOTA_TP_H
+#define _FOTA_TP_H
+
+/******************************************************************************/
+/*                                                                            */
+/*                                  INCLUDES                                  */
+/*                                                                            */
+/******************************************************************************/
+#include "FOTA_TP_Types.h"
+
+/******************************************************************************/
+/*                                                                            */
+/*                                  DEFINES                                   */
+/*                                                                            */
+/******************************************************************************/
+/**
+ * @def           <Define name>
+ * @brief         <Define details>.
+ */
+
+/******************************************************************************/
+/*                                                                            */
+/*                                   ENUMS                                    */
+/*                                                                            */
+/******************************************************************************/
+/**
+ * @enum          <Enum name>
+ * @brief         <Enum details>.
+ */
+
+/******************************************************************************/
+/*                                                                            */
+/*                                 STRUCTURES                                 */
+/*                                                                            */
+/******************************************************************************/
+/**
+ * @struct        <Structure name>
+ * @brief         <Structure details>.
+ */
+
+/******************************************************************************/
+/*                                                                            */
+/*                                   UNIONS                                   */
+/*                                                                            */
+/******************************************************************************/
+/**
+ * @union         <Union name>
+ * @brief         <Union details>.
+ */
+
+/******************************************************************************/
+/*                                                                            */
+/*                              EXTERN VARIABLES                              */
+/*                                                                            */
+/******************************************************************************/
+
+/******************************************************************************/
+/*                                                                            */
+/*                              EXTERN FUNCTIONS                              */
+/*                                                                            */
+/******************************************************************************/
+extern TPError_E ge_TP_ParseCtrlPktList(uint8_t *u8pt_data, uint8_t u8_dataLen, \
+   CPList_T *stpt_CPList);
+extern TPError_E ge_TP_FindCPByType(const CPList_T *stpt_CPList, CPTypes_E e_CPType, \
+   CPBlock_T **stptpt_CPBlock);
+
+#endif //!_FOTA_TP_H
+
+/**
+ * Copyright(c) Bajaj Auto Technology Limited (BATL) as an unpublished work.
+ * THIS SOFTWARE AND/OR MATERIAL IS THE PROPERTY OF BATL.
+ * ALL USE, DISCLOSURE, AND/OR REPRODUCTION NOT SPECIFICALLY AUTHORIZED BY
+ * BATL IS PROHIBITED.
+ *
+ * @author:Shivam Chudasama [SC]
+ */
---
+++ _ASW/BLE_FOTA/BLE_FOTA_TP/FOTA_TP_Types.h
@@ -0,0 +1,145 @@
+/**
+ * @file          FOTA_TP_Types.h
+ * @brief         Header file containing typedefines for FOTA transport protocol.
+ * @date          13/02/2026
+ * @author        Shivam Chudasama [SC]
+ * @copyright     Bajaj Auto Technology Limited (BATL)
+ */
+
+#ifndef _FOTA_TP_TYPES_H
+#define _FOTA_TP_TYPES_H
+
+/******************************************************************************/
+/*                                                                            */
+/*                                  INCLUDES                                  */
+/*                                                                            */
+/******************************************************************************/
+#include <stdint.h>
+
+/******************************************************************************/
+/*                                                                            */
+/*                                  DEFINES                                   */
+/*                                                                            */
+/******************************************************************************/
+/**
+ * @def           MAX_CP_BLOCK_SIZE
+ * @brief         Maximum size of a Control Packet block in bytes.
+ */
+#define MAX_CP_BLOCK_SIZE                    (128U)
+
+/**
+ * @def           MAX_CP_BLOCKS
+ * @brief         Maximum number of Control Packet blocks in one transport packet.
+ */
+#define MAX_CP_BLOCKS                        (10U)
+
+/**
+ * @def           MAX_MTU_SIZE_WITH_DLE
+ * @brief         Maximum size of a MTU (Maximum Transmission Unit) with
+ *                DLE (Data Length Extension) in bytes.
+ */
+#define MAX_MTU_SIZE_WITH_DLE                (244U)
+
+/**
+ * @def           MAX_GATT_DATA_SIZE_WITH_DLE
+ * @brief         Maximum size of a GATT (MTU + Headers) with DLE in bytes.
+ */
+#define MAX_GATT_DATA_SIZE_WITH_DLE          (251U)
+
+/******************************************************************************/
+/*                                                                            */
+/*                                   ENUMS                                    */
+/*                                                                            */
+/******************************************************************************/
+/**
+ * @enum          CPTypes_E
+ * @brief         Enumeration of different Control Packet types.
+ */
+typedef enum
+{
+   eCPT_FOTA_CONTROL = 0x01,                 /**< FOTA Control Packet */
+   eCPT_IMAGE_TYPE = 0x02,                   /**< Firmware Data Packet */
+   eCPT_IMAGE_SIZE = 0x03,                   /**< Image Size Packet */
+   eCPT_TARGET_ECU = 0x04,                   /**< Target ECU Identifier Packet */
+   eCPT_FW_VERSION = 0x05,                   /**< Firmware Version Packet */
+   eCPT_HW_VERSION = 0x06,                   /**< Hardware Version Packet */
+   eCPT_CRC = 0x07,                          /**< Checksum Packet */
+   eCPT_HASH = 0x08,                         /**< Hash Packet */
+   eCPT_SIGNATURE = 0x09,                    /**< Signature Packet */
+   eCPT_DEPENDENCY_INFO = 0x0A,              /**< Dependency Information Packet */
+   // More CP types can be added here as needed
+} CPTypes_E;
+
+/**
+ * @enum          TPError_E
+ * @brief         Enums listing all the possible error codes for Transport Protocol
+ *                APIs.
+ */
+typedef enum
+{
+   eTP_OK,                                   /**< No error. */
+   eTP_INVALID_CP_BLOCK_LENGTH,              /**< Invalid Control Packet block length. */
+   eTP_NO_SUCH_CP_BLOCK_AVAILABLE,           /**< No such Control Packet block available in
+                                                   Control Packet list. */
+} TPError_E;
+
+/******************************************************************************/
+/*                                                                            */
+/*                                 STRUCTURES                                 */
+/*                                                                            */
+/******************************************************************************/
+/**
+ * @struct        CPBlock_T
+ * @brief         Structure containing information of different parameters of
+ *                Control Packet data block.
+ */
+typedef struct
+{
+   CPTypes_E e_CPType;                       /**< Control Packet data type */
+   uint8_t u8_CPBlockLength;                 /**< Length of Control Packet block */
+   uint8_t u8ar_CPData[MAX_CP_BLOCK_SIZE];   /**< Actual Control Packet data */
+} CPBlock_T;
+
+/**
+ * @struct        CPList_T
+ * @brief         Structure containing list of all CP blocks parsed from received
+ *                GATT characteristic data.
+ */
+typedef struct
+{
+   CPBlock_T star_CPBlocks[MAX_CP_BLOCKS];   /**< All the individual CP blocks */
+   uint8_t u8_count;                         /**< Count of total CP blocks */
+} CPList_T;
+
+/******************************************************************************/
+/*                                                                            */
+/*                                   UNIONS                                   */
+/*                                                                            */
+/******************************************************************************/
+/**
+ * @union         <Union name>
+ * @brief         <Union details>.
+ */
+
+/******************************************************************************/
+/*                                                                            */
+/*                              EXTERN VARIABLES                              */
+/*                                                                            */
+/******************************************************************************/
+
+/******************************************************************************/
+/*                                                                            */
+/*                              EXTERN FUNCTIONS                              */
+/*                                                                            */
+/******************************************************************************/
+
+#endif //!_FOTA_TP_TYPES_H
+
+/**
+ * Copyright(c) Bajaj Auto Technology Limited (BATL) as an unpublished work.
+ * THIS SOFTWARE AND/OR MATERIAL IS THE PROPERTY OF BATL.
+ * ALL USE, DISCLOSURE, AND/OR REPRODUCTION NOT SPECIFICALLY AUTHORIZED BY
+ * BATL IS PROHIBITED.
+ *
+ * @author:Shivam Chudasama [SC]
+ */
